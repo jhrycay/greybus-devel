1b7b0ec16bbd98c80ba9b54f93ed95a60b5a1c33
Jim Wylder <jwylder@motorola.com>
2016-12-19 10:20:10 -0600

    staging: greybus: manifest: Add VID/PID field
    Add the ability to parse a manifest with the VID/PID entry.
    These fields can be validated upstream.
    
    Signed-off-by: Jim Wylder <jwylder@motorola.com>

--- drivers/staging/greybus/greybus_manifest.h
+++ drivers/staging/greybus/greybus_manifest.h
@@ -19,6 +19,7 @@ enum greybus_descriptor_type {
 	GREYBUS_TYPE_STRING		= 0x02,
 	GREYBUS_TYPE_BUNDLE		= 0x03,
 	GREYBUS_TYPE_CPORT		= 0x04,
+	GREYBUS_TYPE_IDS		= 0x05,
 };
 
 enum greybus_protocol {
@@ -153,6 +154,14 @@ struct greybus_descriptor_header {
 	__u8	pad;
 } __packed;
 
+/*
+ * Descriptor containing the vendor ID and product ID
+ */
+struct greybus_descriptor_ids {
+	__u32   vid;
+	__u32   pid;
+} __packed;
+
 struct greybus_descriptor {
 	struct greybus_descriptor_header		header;
 	union {
@@ -160,6 +169,7 @@ struct greybus_descriptor {
 		struct greybus_descriptor_interface	interface;
 		struct greybus_descriptor_bundle	bundle;
 		struct greybus_descriptor_cport		cport;
+		struct greybus_descriptor_ids		ids;
 	};
 } __packed;
 
